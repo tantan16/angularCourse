4d6369c2eccacb19a86e276595074f1cdfe3d669
tantan16 <tania.elizalde.vargas@gmail.com>
2024-04-04 12:32:02 -0600

    Form Groups excercise

--- src/app/formularios/formularios.component.ts
+++ src/app/formularios/formularios.component.ts
@@ -1,16 +1,19 @@
-import { Component } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-formularios',
   templateUrl: './formularios.component.html',
   styleUrls: ['./formularios.component.scss']
 })
-export class FormulariosComponent {
-  name = new FormControl('', [Validators.minLength(4), Validators.required]);
-  isChecked = new FormControl(false);
+
+export class FormulariosComponent implements OnInit{
+  form = new FormGroup({
+    name: new FormControl('',[Validators.minLength(3), Validators.required]),
+    isChecked: new FormControl(true),
+  });
   constructor(){}
-  ngOnInit(): void{
-    //this.name.
-  }
+
+  ngOnInit():void{}
 }
+
